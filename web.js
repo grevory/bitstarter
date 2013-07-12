64555e5fc36696b8b9dd7dfc23e5c1c0ba40645c
Gregory Pike <greg@gregpike.ca>
2013-07-12 02:19:08 -0400

    Added index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var data = fs.readFileSync('index.html');
+  var bufferedContent = new Buffer(data);
+  response.send(bufferedContent.toString());
 });
 
 var port = process.env.PORT || 5000;
