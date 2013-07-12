3abdde23ed82e0a9db0e1dd6bf34abc54de7fc14
Gregory Pike <greg@gregpike.ca>
2013-07-12 03:18:46 -0400

    Updated grader to accept a URL

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -44,8 +45,12 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(htmlfile, checksfile, isRawHtml) {
+    if (!!isRawHtml) {
+    	$ = cheerio.load(htmlfile);
+    } else {
+    	$ = cheerioHtmlFile(htmlfile);
+    }
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,8 +70,25 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <app_url>', 'URL of app')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+    var isRawHtml = false;
+    var html = program.file;
+
+    if (!!program.url) {
+    	rest.get(program.url).on('complete', function(result) {
+		  if (result instanceof Error) {
+		    sys.puts('Error: ' + result.message);
+		    this.retry(5000); // try again after 5 sec
+		  } else {
+		    isRawHtml = true;
+		    html = result;
+		  }
+		});
+    }
+
+    var checkJson = checkHtmlFile(html, program.checks, isRawHtml);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
